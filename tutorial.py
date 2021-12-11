3d67ede8f42aa215227a1eb92661d226e8af253c
leeminze214 <leeminz214@gmail.com>
2021-12-10 21:44:30 -0500

    first commit

---
+++ tutorial.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route("/home")
+def home_page():
+    return render_template('home.html')
+
+@app.route("/about")
+def page_page():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
